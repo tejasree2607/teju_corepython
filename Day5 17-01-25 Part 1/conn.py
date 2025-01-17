59b775de0075ab38a3fab2cdb37a2dddd367334c
tejasree2607 <modalatejasree@gmail.com>
2025-01-17 10:58:05 +0530

    Create conn.py

---
+++ Day5 17-01-25 Part 1/conn.py
@@ -0,0 +1,13 @@
+import mysql.connector as c
+mydb=c.connect(
+    host='localhost',
+    user='root',
+    password='root',
+    database='coe'
+)
+mycursor=mydb.cursor()
+sql='select * from mini'
+mycursor.execute(sql)
+res=mycursor.fetchall()
+for x in res:
+    print(x)
